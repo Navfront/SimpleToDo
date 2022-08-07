ccbe8678dc61795133e31115b0695576057dfcd5
Navfront <b3rt@yandex.ru>
2022-08-07 12:00:18 +0300

    uuid and offline adding todo

--- src/components/ui/todo-list/todo-list.tsx
+++ src/components/ui/todo-list/todo-list.tsx
@@ -4,13 +4,36 @@ import loadingIcon from '../todo-card/loading.gif'
 import { useAppSelector } from '../../../redux/redux-hooks'
 import { useEffect } from 'react'
 import { useAppDispatch } from './../../../redux/redux-hooks'
-import { getTodos } from './../../../redux/slices/todosSlice'
+import { getTodos, Todo } from './../../../redux/slices/todosSlice'
 
 function TodoList () {
   const isLoading = useAppSelector((state) => state.app.isLoading)
-  const todos = useAppSelector((state) => state.todos.todos)
+  const onlineTodos = useAppSelector((state) => state.todos.todos)
+  const offlineTodos = useAppSelector(state => state.offline.todos)
   const { isAuth, userName } = useAppSelector(state => state.auth)
   const dispatch = useAppDispatch()
+
+  const renderTodos = (isOnline: boolean) => {
+    const Empty = <p>Add your first TODO!</p>
+
+    const createTodos = (todos: Todo[]) => {
+      return todos && todos.length > 0
+        ? todos.map((todo) => (
+          <li key={todo.todoId}>
+            <TodoCard todoId={todo.todoId} title={todo.title} isDone={todo.isDone} />
+          </li>
+        ))
+        : Empty
+    }
+
+    if (isOnline) {
+      console.log('online render')
+      return createTodos(onlineTodos)
+    } else {
+      console.log('offline render')
+      return createTodos(offlineTodos)
+    }
+  }
 
   useEffect(() => {
     console.log('auth is ', isAuth)
@@ -25,14 +48,8 @@ function TodoList () {
     <img src={loadingIcon} alt="loading icon" style={{ margin: '0 auto', display: 'block' }} />
       )
     : (
-    <StyledTodoList>
-      {todos && todos.length > 0
-        ? todos.map((todo) => (
-            <li key={todo.todoId}>
-              <TodoCard todoId={todo.todoId} title={todo.title} isDone={todo.isDone} />
-            </li>
-        ))
-        : <p>List is empty...</p> }
+      <StyledTodoList>
+        {renderTodos(isAuth)}
     </StyledTodoList>
       )
 }
