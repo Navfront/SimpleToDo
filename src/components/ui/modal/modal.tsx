6fce868a3c3cbc031ab5609b25c788af91728760
Navfront <b3rt@yandex.ru>
2022-08-07 11:36:04 +0300

    connect modal to state

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,10 +1,12 @@
 
 import { PropsWithChildren } from 'react'
 import { createPortal } from 'react-dom'
+import { useAppSelector } from '../../../redux/redux-hooks'
 import { StyledModal } from './styled'
 
 function Modal ({ children }: PropsWithChildren) {
-  return createPortal(<StyledModal>{children}</StyledModal>, document.getElementById('modal')!)
+  const isModalShow = useAppSelector(state => state.app.isModalShow)
+  return isModalShow ? createPortal(<StyledModal>{children}</StyledModal>, document.getElementById('modal')!) : <></>
 }
 
 export default Modal
