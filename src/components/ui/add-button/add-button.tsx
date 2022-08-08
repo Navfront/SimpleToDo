c56e04d1f9b2c91717144ff7965128759deab224
Navfront <b3rt@yandex.ru>
2022-08-09 00:39:05 +0300

    add button design

--- src/components/ui/add-button/add-button.tsx
+++ src/components/ui/add-button/add-button.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from 'react'
 import { useAppSelector } from '../../../redux/redux-hooks'
-import { StyledAddTodoBtn } from './styled'
 import { useAppDispatch } from './../../../redux/redux-hooks'
 import { addOffline } from '../../../redux/slices/offlineSlice'
 import { v4 as uuidv4 } from 'uuid'
 import { sagaAddTodo } from '../../../redux/saga/saga-actions'
+import { Button } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
 
 function AddButton ({ children }:PropsWithChildren) {
   const { isAuth } = useAppSelector(state => state.auth)
@@ -17,7 +18,7 @@ function AddButton ({ children }:PropsWithChildren) {
       dispatch(addOffline({ todoId: uuidv4(), title: 'New ToDo', isDone: false }))
     }
   }
-  return <StyledAddTodoBtn type="button" onClick={() => { onClickHandler() }}>{children}</StyledAddTodoBtn>
+  return <Button type="primary" shape='round' size='large' onClick={() => { onClickHandler() }}>Add <PlusOutlined /></Button>
 }
 
 export default AddButton
